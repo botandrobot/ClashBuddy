65f7781240e2e7317cba6e58111ffdd8e41b9595
tokenbuddy42 <token@honorbuddy.com>
2017-07-13 11:39:09 +0200

    Added a Sequence and a Random Action Selector. The Sequence Action Selector is good for Handling the Tutorial.

---
+++ src/Buddy.Clash.DefaultSelectors/RandomActionSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buddy.Clash.DefaultSelectors
+{
+	using System;
+	using Engine;
+
+	class RandomActionSelector : IActionSelector
+    {
+	    public string Name => "Random Selector";
+
+	    public string Description => "Will try to cast a random spell every tick.";
+
+	    public string Author => "Token";
+
+	    public Version Version => new Version(1, 0, 0, 0);
+	    public Guid Identifier => new Guid("{41989DD3-6F76-4794-8870-4A1E860A0EF5}");
+
+	    private static readonly Random _generator = new Random();
+
+	    public CastRequest GetNextCast()
+	    {
+		    var battle = ClashEngine.Instance.Battle;
+		    if (battle == null || !battle.IsValid) return null;
+
+		    var spells = ClashEngine.Instance.AvailableSpells;
+
+		    var spell = spells[_generator.Next(4)];
+		    if (spell == null || !spell.IsValid) return null;
+		    return new CastRequest(spell.Name.Value, battle.SummonerTowers[0].StartPosition);
+	    }
+    }
+}
---
+++ src/Buddy.Clash.DefaultSelectors/SequenceActionSelector.cs
@@ -0,0 +1,33 @@
+﻿namespace Buddy.Clash.DefaultSelectors
+{
+	using System;
+	using Engine;
+
+	public class SequenceActionSelector : IActionSelector
+    {
+	    public string Name => "Sequence Selector";
+
+	    public string Description => "Will try to cast a spell in sequence every tick.";
+
+	    public string Author => "Token";
+
+	    public Version Version => new Version(1, 0, 0, 0);
+	    public Guid Identifier => new Guid("{7110C08C-283B-4D2F-AF26-982DD2246024}");
+
+	    private int currentSpell = 0;
+
+	    public CastRequest GetNextCast()
+	    {
+		    var battle = ClashEngine.Instance.Battle;
+		    if (battle == null || !battle.IsValid) return null;
+
+		    var spells = ClashEngine.Instance.AvailableSpells;
+		    if (currentSpell >= 4 || currentSpell < 0)
+			    currentSpell = 0;
+			
+			var spell = spells[currentSpell++];
+			if (spell == null || !spell.IsValid) return null;
+			return new CastRequest(spell.Name.Value, battle.SummonerTowers[0].StartPosition);
+	    }
+    }
+}
