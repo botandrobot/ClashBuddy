8d414af9d5f03e46d8f6873f98de21e9b31c8489
Rene Windegger <rene@windegger.wtf>
2017-09-01 18:40:51 +0200

    Added the missing field to the settings.

--- src/Buddy.Clash.DefaultSelectors/Settings/ApolloSettings.cs
+++ src/Buddy.Clash.DefaultSelectors/Settings/ApolloSettings.cs
@@ -15,49 +15,57 @@ namespace Buddy.Clash.DefaultSelectors.Settings
 	    }
 
         [DefaultValue(Player.FightStyle.Balanced)]
-        [SettingsName("Choose Apollos fight style")]
-        [SettingsDescription("Smart balanced, concentrated on the Defense or as an angry rusher?")]
+        [SettingsName("Fight Style")]
+        [SettingsDescription("Choose Apollos fight style. Smart balanced, concentrated on the Defense or as an angry rusher?")]
         public Player.FightStyle FightStyle { get; set; }
 
         [SettingsName("Random Deployment Faktor")]
         [SettingsDescription("Random deployment factor range to let the bot look more human like")]
-        [FloatRangeSettings(100, 0, 10000)]
+        [IntegerRangeSettings(100, 0, 10000)]
         [DefaultValue(200)]
         public int RandomDeploymentValue { get; set; }
 
         [SettingsName("Enemys KingTower spell damaging mode")]
         [SettingsDescription("Starts to attack the enemys KingTower with all damaging spells at the Health...")]
-        [FloatRangeSettings(100, 0, 10000)]
+        [IntegerRangeSettings(100, 0, 10000)]
         [DefaultValue(400)]
         public int KingTowerSpellDamagingHealth { get; set; }
 
         [SettingsName("Spell position correction (friendly chars around)")]
         [SettingsDescription("Don´t correct spell deployment position if at least x friendly characters around the enemy")]
-        [FloatRangeSettings(0, 0, 50)]
+        [IntegerRangeSettings(1, 0, 50)]
         [DefaultValue(2)]
         public int SpellCorrectionConditionCharCount { get; set; }
 
         [SettingsName("Spell deployment decision (minimum anzahl Characters)")]
         [SettingsDescription("How many enemy characters should be at least in the area for an deploy")]
-        [FloatRangeSettings(0, 0, 50)]
+        [IntegerRangeSettings(1, 0, 50)]
         [DefaultValue(5)]
         public int SpellDeployConditionCharCount { get; set; }
 
-        [SettingsName("Game-Beginning: Mana-Load")]
+	    [SettingsGroup("Game Start")]
+		[SettingsName("Mana-Load")]
         [SettingsDescription("How much Mana till first attack")]
-        [FloatRangeSettings(0, 0, 10)]
+        [IntegerRangeSettings(1, 0, 10)]
         [DefaultValue(9)]
         public int ManaTillFirstAttack { get; set; }
 
-        [SettingsName("Tank health")]
+	    [SettingsGroup("Attack")]
+	    [SettingsName("Mana-Load")]
+	    [SettingsDescription("How much Mana till first attack")]
+	    [IntegerRangeSettings(1, 0, 10)]
+	    [DefaultValue(7)]
+	    public int ManaTillAttack { get; set; }
+
+		[SettingsName("Tank health")]
         [SettingsDescription("How much health-points to classify an character as a tank")]
-        [FloatRangeSettings(0, 0, 10000)]
+        [IntegerRangeSettings(10, 0, 10000)]
         [DefaultValue(1200)]
         public int MinHealthAsTank { get; set; }
 
-        //[SettingsGroup("Random Deployment Faktor")]
-        //[FloatRangeSettings(100, 0, 10000)]
-        //[DefaultValue(200)]
-        //public int RandomDeploymentValue { get; set; }
-    }
+		//[SettingsGroup("Random Deployment Faktor")]
+		//[FloatRangeSettings(100, 0, 10000)]
+		//[DefaultValue(200)]
+		//public int RandomDeploymentValue { get; set; }
+	}
 }
